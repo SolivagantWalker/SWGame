7e9ab231c8a70cf9542ed0399418443e41659522
SolivagantWalker <SolivagantWalker@DESKTOP-ABKH3ST>
2022-11-27 21:32:05 +0100

    "GLFW and GLAD integraton"

--- SWGame/main.cpp
+++ SWGame/main.cpp
@@ -1,8 +1,46 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
 #include "Application.hpp"
 
-int main() {
+static int width = 640;
+static int height = 480;
 
-	std::cout << Application::Init();
-	return 0;
+int main(void)
+{
+    GLFWwindow* window;
+
+    if (!glfwInit())
+        exit(EXIT_FAILURE);
+
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
+
+    window = glfwCreateWindow(640, 480, "Window", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        exit(EXIT_FAILURE);
+    }
+
+
+    glfwMakeContextCurrent(window);
+    gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+    glfwSwapInterval(1);
+
+    // NOTE: OpenGL error checks have been omitted for brevity
+
+    while (!glfwWindowShouldClose(window))
+    {
+        glViewport(0, 0, width, height);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    glfwDestroyWindow(window);
+
+    glfwTerminate();
+    exit(EXIT_SUCCESS);
 }
